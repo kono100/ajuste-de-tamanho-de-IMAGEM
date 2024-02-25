a9e3de0d1043e366a8ad1d084e02aac32a89f122
Thiago Campos <thiago-kono@hotmail.com>
2024-02-25 12:23:15 -0300

    add arquivos att

--- main.py
+++ main.py
@@ -13,23 +13,24 @@ def redimensionar_imagem(input_path, output_path, novo_tamanho):
 
 # Diretórios de entrada e saída
 caminho_entrada = 'img/t.jpg'
-tamanho_desejado = (333, 100)
+tamanho_desejado = (5,6)
+tamanho_str = f"{tamanho_desejado[0]}x{tamanho_desejado[1]}"
+
 
 # Caminho de saída na pasta Ajustada
 nome_arquivo = os.path.splitext(os.path.basename(caminho_entrada))[0]  # Obtém o nome do arquivo sem extensão
-caminho_saida = os.path.join('Ajustada', f'{nome_arquivo}(SF).png')
+caminho_saida = os.path.join('Ajustada', f'{nome_arquivo}  {tamanho_str}.png')
 
 # Redimensionar a imagem
 redimensionar_imagem(caminho_entrada, caminho_saida, tamanho_desejado)
 
 # Exibir informações
 print(f"\nSalvo como: {caminho_saida}\n")
-print(f"{tamanho_desejado}")
 
 # Verificar se já existe um arquivo com o mesmo nome e adicionar um sufixo numerado
 counter = 1
 while os.path.exists(caminho_saida):
-    output_filename = f'{nome_arquivo}(SF{counter}).png'
+    output_filename = f'{nome_arquivo}  {tamanho_str}  ({counter}).png'
     caminho_saida = os.path.join('Ajustada', output_filename)
     counter += 1
 
