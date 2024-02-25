02519528ea99706a64f29d447c23422c1cfd54d0
Thiago Campos <thiago-kono@hotmail.com>
2024-02-25 12:39:23 -0300

    add arquivos att top

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ from PIL import Image
 import os
 
 def redimensionar_imagem(input_path, output_path, novo_tamanho):
+    # Verificar se o arquivo existe
+    if not os.path.exists(input_path):
+        raise FileNotFoundError(f'A imagem {input_path} não foi encontrada.')
+
     # Abrir a imagem
     imagem = Image.open(input_path)
 
@@ -14,23 +18,24 @@ def redimensionar_imagem(input_path, output_path, novo_tamanho):
 # Diretórios de entrada e saída
 caminho_entrada = 'img/Imagem.jpg'
 
-tamanho_desejado = "1000x641"
+tamanho_desejado = "1080x641"
 tamanho_desejado = tamanho_desejado.replace("x", ",")
 tamanho_desejado = tuple(map(int, tamanho_desejado.split(",")))
 
-
 tamanho_str = f"{tamanho_desejado[0]}x{tamanho_desejado[1]}"
-
 
 # Caminho de saída na pasta Ajustada
 nome_arquivo = os.path.splitext(os.path.basename(caminho_entrada))[0]  # Obtém o nome do arquivo sem extensão
 caminho_saida = os.path.join('Ajustada', f'{nome_arquivo}  {tamanho_str}.png')
 
 # Redimensionar a imagem
-redimensionar_imagem(caminho_entrada, caminho_saida, tamanho_desejado)
-
-# Exibir informações
-print(f"\nSalvo como: {caminho_saida}\n")
+try:
+    redimensionar_imagem(caminho_entrada, caminho_saida, tamanho_desejado)
+    # Exibir informações
+    print(f"\nSalvo como: {caminho_saida}\n")
+except FileNotFoundError as e:
+    # Imprimir mensagem de erro
+    print(f"Erro: {e}")
 
 # Verificar se já existe um arquivo com o mesmo nome e adicionar um sufixo numerado
 counter = 1
@@ -40,7 +45,10 @@ while os.path.exists(caminho_saida):
     counter += 1
 
 # Redimensionar novamente e salvar com o novo nome
-redimensionar_imagem(caminho_entrada, caminho_saida, tamanho_desejado)
-
-# Exibir novo nome do arquivo
-print(f"\nSalvo como: {caminho_saida}\n")
+try:
+    redimensionar_imagem(caminho_entrada, caminho_saida, tamanho_desejado)
+    # Exibir novo nome do arquivo
+    print(f"\nSalvo como: {caminho_saida}\n")
+except FileNotFoundError as e:
+    # Imprimir mensagem de erro
+    print(f"Erro: {e}")
